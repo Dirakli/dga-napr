7622d81a94ff122b360eb507cfab84c52ca34897
Dirakli_ <dgebuadzeirakli@gmail.com>
2023-11-24 17:19:18 +0400

    correction and responsive of header

--- app/components/main/Header.tsx
+++ app/components/main/Header.tsx
@@ -127,7 +127,7 @@ function Header() {
           />
           <Image
             className="bg-graycl sm:hidden flex p-1 cursor-pointer"
-            src={nightMode ? "/icons/More.svg" : "/icons/MoreDarkmode.svg"}
+            src={nightMode ? "/icons/More.svg" : "/icons/MoreDa rkmode.svg"}
             alt="Icon gives more options"
             width={25}
             height={30}
